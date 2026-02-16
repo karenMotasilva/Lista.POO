b6b4cb29ba5a494b4bff381eec9b5c32cae3376b
NilsonBatista-07 <givanilson.off@gmail.com>
2026-02-16 20:06:43 -0300

    atualicação da atividade 6

--- atividade6.js
+++ atividade6.js
@@ -1,29 +1,78 @@
 class Cliente {
-    nome
-    cpf
-    dataNascimento
-    endereco
-    email 
-    ocupacao 
-    rendaAnual 
-    nivelDeEscolaridade
+    #nome
+    #cpf
+    #dataNascimento
+    #endereco
+    #email 
+    #ocupacao 
+    #rendaAnual 
+    #nivelDeEscolaridade
 
     constructor (nome, cpf, dataNascimento, endereco, email, ocupacao, rendaAnual, nivelDeEscolaridade) {
-        this.nome = nome
-        this.cpf = cpf
-        this.dataNascimento = dataNascimento
-        this.Endereço = endereco
-        this.email= email
-        this.ocupacao = ocupacao
-        this.rendaAnual = rendaAnual
-        this.nívelDeEscolaridade =nivelDeEscolaridade
+        this.#nome = nome
+        this.#cpf = cpf
+        this.#dataNascimento = dataNascimento
+        this.#endereco = endereco
+        this.#email= email
+        this.#ocupacao = ocupacao
+        this.#rendaAnual = rendaAnual
+        this.#nivelDeEscolaridade =nivelDeEscolaridade
 
     }
+    get nome(){
+        return`Nome : ${this.#nome}`
+    }
+    get cpf (){
+        return `CPF: ${this.#cpf}`
+    }
+    get  dataNascimento (){
+        return `Data de Nascimento: ${this.#dataNascimento}`
+    }
+     get  endereco (){
+        return `Endereço: ${this.#endereco}`
+    }
+    get  email (){
+        return `Email: ${this.#email}`
+    }
+     get  ocupacao (){
+        return `Oculpação: ${this.#ocupacao}`
+    }
+     get  rendaAnual (){
+        return `Renda Anual${this.#rendaAnual}`
+    } 
+    get nivelDeEscolaridade  (){
+        return `Nivel: ${this.#nivelDeEscolaridade}`
+    }
+    set nome (novoNome){
+        this.#nome(novoNome)
+    }
+    set cpf (novocpf){
+        this.#cpf (novocpf)
+    }
+    set  dataNascimento(novaData){
+        this.#dataNascimento (novaData)
+    }
+    set  endereco(novoEndereco){
+        this.#endereco(novoEndereco)
+    }
+    set email (novoEmail){
+        this.#email (novoEmail)
+    }
+    set ocupacao (novaOcupacao){
+        this.#ocupacao(novaOcupacao)
+    }
+    set  rendaAnual(novaRenda){
+        this.#rendaAnual (novaRenda)
+    }
+    set  nivelDeEscolaridade(novoNivel){
+        this.#nivelDeEscolaridade(novoNivel)
+    }
+
     relatorioDoCliente(){
-        return`Relatorio Do Cliente \n Nome: ${this.nome} \n CPF: ${this.cpf}\n Data de Nascimento ${this.dataNascimento}
-        Endereço: ${this.endereco}\n E-mail: ${this.email} \n Ocupação: ${this.ocupacao} \n Renda: ${this.rendaAnual}
-        Nivel de Escolaridade: ${this.nivelDeEscolaridade}`
+        return`Relatorio Do Cliente \n Nome: ${this.#nome} \n CPF: ${this.#cpf}\n Data de Nascimento ${this.#dataNascimento}
+        Endereço: ${this.#endereco}\n E-mail: ${this.#email} \n Ocupação: ${this.#ocupacao} \n Renda: ${this.#rendaAnual}
+        Nivel de Escolaridade: ${this.#nivelDeEscolaridade}`
     }
 }
 let cliente = new Cliente("nilson",'01230','07-05-1993',"rua viva","nilson@nilson","dev","2500","superior" )
-console.log(cliente)
+console.log(cliente.nome)
